1640bd8321c72d7aef42f7a3fac91fc4b894638a
AshGw <ashrefgw@proton.me>
2024-09-14 11:21:59 +0100

    add funcs

---
+++ funcs.zsh
@@ -0,0 +1,123 @@
+precmd() {
+    # Print the previously configured title
+    print -Pnr -- "$TERM_TITLE"
+
+    # Print a new line before the prompt, but only if it is not the first line
+    if [ "$NEWLINE_BEFORE_PROMPT" = yes ]; then
+        if [ -z "$_NEW_LINE_BEFORE_PROMPT" ]; then
+            _NEW_LINE_BEFORE_PROMPT=1
+        else
+            print ""
+        fi
+    fi
+}
+# Needs SSH
+# generate new SSH keys for github, run this u'll get the pub key copied to ur clipboard,just paste it
+ghkey() {
+ bash <(curl -L https://raw.githubusercontent.com/AshGw/dotfiles/main/.ssh/_gh_gen.sh )
+}
+
+# Needs docker
+# terminate all running conatainers
+tercon() {
+	for c in $(docker ps -a | tail -n+2 | awk '{print $1}'); do
+  		docker stop "${c}" || :
+  		docker rm "${c}"
+	done
+}
+
+# remove all volumes
+tervol() {
+   docker volume rm $(docker volume ls -q)
+}
+
+# remove all images
+terimg() {
+   for img in $(docker images -q); do
+        docker rmi "${img}" || :
+    done
+}
+
+dprune() {
+	 tercon && terimg && tervol
+   docker container prune -f
+   docker system prune -f
+   docker image prune -f
+   docker volume prune -f
+}
+
+# shows pretty `man` page.
+man () {
+  env \
+    LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+    LESS_TERMCAP_md=$(printf "\e[1;31m") \
+    LESS_TERMCAP_me=$(printf "\e[0m") \
+    LESS_TERMCAP_se=$(printf "\e[0m") \
+    LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+    LESS_TERMCAP_ue=$(printf "\e[0m") \
+    LESS_TERMCAP_us=$(printf "\e[1;32m") \
+      man "$@"
+}
+
+# create a new directory & cd into it
+mdd () {
+ mkdir -p "$@" && cd "$@"
+}
+
+# This needs my GPG key
+# encrypt a file with a passphrase
+passenc() {
+    local input_file=$1
+    local output_file="${input_file}.gpg"
+    if gpg --symmetric --cipher-algo AES256 --quiet --batch --yes --output "$output_file" "$input_file"; then
+        shred -u "$input_file"
+        echo -e "\e[1;32mEncrypted $input_file and saved as: $output_file\e[0m"
+    else
+        echo -e "\e[1;31mEncryption failed for $input_file\e[0m"
+    fi
+}
+
+passdec() {
+    local input_file=$1
+    local output_file="${input_file%.gpg}"
+
+    if gpg --use-agent --quiet --batch --yes --decrypt --cipher-algo AES256 --output "$output_file" "$input_file" 2>/dev/null; then
+        shred -u "$input_file"
+        echo -e "\e[1;32mDecrypted $input_file and saved as: $output_file\e[0m"
+    else
+        echo -e "\e[1;31mDecryption failed for $input_file\e[0m"
+    fi
+}
+
+# copies the content of a file to the clipboard
+cpf() {
+    if [[ -n $1 && -f $1 ]]; then
+        xclip -selection clipboard < $1
+        echo -e "\e[1;32mContents of '$1' copied to clipboard.\e[0m"
+    else
+        echo "Usage: cpf <filename>"
+    fi
+}
+# Needs xclip
+# short for copy command, copies the output of the command to the clipboard
+ccmd() {
+    "$@" | xclip -selection clipboard
+}
+
+# When the gpg dameon fucking up in TTY, you gotta lock in
+loadpg() {
+   pkill -9 gpg-agent
+   export GPG_TTY=$(tty)
+}
+
+export MY_CURRENT_FUCKING_CITY=Tunis # TODO: Upgrade
+weather() {
+  LOCATION="${MY_CURRENT_FUCKING_CITY}"
+    printf "%s" "$SEP1"
+    if [ "$IDENTIFIER" = "unicode" ]; then
+        printf "%s" "$(curl -s wttr.in/$LOCATION?format=1)"
+    else
+        printf "WEA %s" "$(curl -s wttr.in/$LOCATION?format=1 | grep -o "[0-9].*")"
+    fi
+    printf "%s\n" "$SEP2"
+}
